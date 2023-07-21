bfacc9afdff788f0be79b075753865217bd69da3
Maxime GILLEN <m.gillen@groupeonepoint.com>
2023-07-21 14:04:08 +0200

    fix: bypass cors policy

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,12 +1,11 @@
 import fp from 'fastify-plugin'
 import cors from '@fastify/cors'
 import { FastifyRequest } from 'fastify/types/request'
-import envConfig from '../configs/envConfig'
 
 export default fp(async (fastify) => {
   fastify.register(cors, () => (req: FastifyRequest, callback: any) => {
     const corsOptions = {
-      origin: envConfig.NODE_ENV === 'dev' || envConfig.NODE_ENV === 'local',
+      origin: true,
     }
 
     // callback expects two parameters: error and options
